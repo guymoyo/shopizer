9c7e8a92fb4aa1eaf9e8e8a4b85ce45b06006fb2
gmo <Marguerite1960>
2021-07-25 10:07:17 +0200

    system service

--- devops/bd.sh
+++ devops/bd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #service Bande dessine
 
-cd ~/projets/bd/
+cd ~/projets/bd/sm-shop/
 
 nohup mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=9092 -Dspring-boot.run.profiles=prod-bd > trace.log &
--- devops/vt.sh
+++ devops/vt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #service vetement
 
-cd ~/projets/vt/
+cd ~/projets/vt/sm-shop/
 
 nohup mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=8082 -Dspring-boot.run.profiles=prod-vt > trace.log &
